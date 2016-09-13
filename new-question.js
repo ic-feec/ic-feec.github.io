8f842a0f7484ebdf6662401e3c371065c75b3827
thaissacandella <thaissa.ivrit@gmail.com>
2016-09-13 18:00:01 -0300

    new question page

--- new-question.js
+++ new-question.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 	new Page('new-question', function(question){
 		this.main.append($("<h3>New question</h3>"));
-	})
+		this.main.append($('<input type="text" class="title" placeholder="Title" />'));
+		this.main.append($('<input type="text" class="question" placeholder="Question" />'));
+
+		var button = $('<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>');
+		button.on('click', function () {
+			yawp('/questions').create({
+				title : this.main.find('.title').val(),
+				question : this.main.find('.question').val(),
+				answers : [ 'asd', '123', 'hue' ],
+				finished : true
+			}).then(function (q) {
+				console.log('success', q);
+			});
+		});
+		this.main.append(button);
+	});
 });

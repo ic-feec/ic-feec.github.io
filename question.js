e56370c740f6601852f1750f7af6aec842ea072b
Luan Nico <thaissa.ivrit@gmail.com>
2016-09-14 17:05:12 -0300

    better question screen

--- question.js
+++ question.js
@@ -1,14 +1,35 @@
 $(document).ready(function () {
+
+	var format = function (num3) {
+		return parseFloat(Math.round(num3 * 100) / 100).toFixed(2);
+	};
+
 	new Page('question', function(parameter){
 		yawp('/questions/' + parameter).fetch(function(question){
 			var content = $("<div></div>");
+			content.addClass('card');
 			content.append($("<h3>" + question.question + "</h3>"));
-			content.append($('<div>' + JSON.stringify(question.answers) + '</div>'));
-			content.append($('<div class="result">Loading...</div>'))
-			yawp(question.id).get('results').then(function (data) {
-				content.find('.result').html(JSON.stringify(data));
+			var ul = $('<ul>');
+			question.answers.forEach(function (answer, i) {
+				ul.append($('<li class="' + i + '">' + answer + ' <span class="result">0%</span></li>'));
 			});
-			this.main.append(content);
+			content.append(ul);
+			content.append($('<div class="loading">Loading...</div>'));
+			yawp(question.id).get('results').then(function (data) {
+				content.find('.loading').html('');
+				data.forEach(function (datum, i) {
+					console.log(i, datum);
+					content.find('ul li.' + i + ' .result').html(format(datum * 100) + '%');
+				});
+			});
+
+			var cow = $('<div class="col s10 m8 questions">');
+			cow.append(content);
+
+			var row = $('<div class="row">');
+			row.append(cow);
+
+			this.main.append(row);
 		}.bind(this));
 		
 			
